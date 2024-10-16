6f1cb3c112053fbc358006c6ebc9c2ae7be93196
Amaliebra <amaliebratland@gmail.com>
2024-10-16 20:49:34 +0200

    Added class

--- classes/quizList.cs
+++ classes/quizList.cs
@@ -8,9 +8,14 @@ public class quizList
     public void AddQuestion(int questionId, string question)
     {
         quizQuestion.Add(1, "What is the last thing Hodor says?");
-        correctAnswers.Add("")
-        quizQuestion.Add(2, "");
-        quizQuestion.Add(3, "");
+        correctAnswers.Add(1, "Hodor");
+        //---------------------
+        quizQuestion.Add(2, "Who is the Lord Commander of the Night's Watch before Jon Snow?");
+        correctAnswers.Add(2, "Jeor Mormont");
+        //---------------------
+        quizQuestion.Add(3, "What is the name of Daenerys green dragon?");
+        correctAnswers.Add(3, "Rhaegal");
+        //---------------------
         quizQuestion.Add(4, "");
         quizQuestion.Add(5, "");
         quizQuestion.Add(6, "");
@@ -27,7 +32,18 @@ public class quizList
         quizAnswers[1].Add("Hodor");
         quizAnswers[1].Add("Hold the door");
         quizAnswers[1].Add("i'm holding the door");
-        quizAnswers[1].Add("Hodor");
+        quizAnswers[1].Add("Save me");
         //--------------------------
+        quizAnswers[2].Add("Jorah Mormont");
+        quizAnswers[2].Add("Aemon");
+        quizAnswers[2].Add("Jeor Mormont");
+        quizAnswers[2].Add("Alliser Thorne");
+        //-------------------------
+        quizAnswers[3].Add("Viserion");
+        quizAnswers[3].Add("Viserys");
+        quizAnswers[3].Add("Rhaegal");
+        quizAnswers[3].Add("Drogon");
+        //-------------------------
+
     }
 }
